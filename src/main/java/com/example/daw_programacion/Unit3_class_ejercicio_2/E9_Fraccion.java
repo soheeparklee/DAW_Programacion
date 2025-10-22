b392e239d00591ed8a90e9399b3e0af4520d2f12
soheeparklee <soheeparklee@gmail.com>
2025-10-22 19:48:36 +0200

    Unit3_Ejercicio_9

--- src/main/java/com/example/daw_programacion/Unit3_class_ejercicio_2/E9_Fraccion.java
+++ src/main/java/com/example/daw_programacion/Unit3_class_ejercicio_2/E9_Fraccion.java
@@ -1,4 +1,24 @@
 package com.example.daw_programacion.Unit3_class_ejercicio_2;
 
 public class E9_Fraccion {
+    private int num;
+    private int den;
+
+    public E9_Fraccion(){
+        this.num = 0;
+        this.den = 1;
+    }
+    public E9_Fraccion(int num){
+        this.num = num;
+        this.den = 1;
+    }
+
+    public E9_Fraccion(int num, int den){
+        this.num = num;
+        this.den = den;
+    }
+
+    public E9_Fraccion sumar(E9_Fraccion f){
+        return f;
+    }
 }
