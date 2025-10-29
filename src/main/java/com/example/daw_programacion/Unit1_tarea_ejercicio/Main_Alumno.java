95aea8b1108d5beb1c9df6aeee0bea7c1f95292f
Vespertino <soheeparklee@gmail.com>
2025-10-29 19:49:49 +0100

    Unit1_Tarea

---
+++ src/main/java/com/example/daw_programacion/Unit1_tarea_ejercicio/Main_Alumno.java
@@ -0,0 +1,8 @@
+package com.example.daw_programacion.Unit1_tarea_ejercicio;
+
+public class Main_Alumno {
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno("Ana", "Bueno", Turno.TARDE);
+        System.out.println(alumno1.toString());
+    }
+}
---
+++ src/main/java/com/example/daw_programacion/Unit1_tarea_ejercicio/Turno.java
@@ -0,0 +1,5 @@
+package com.example.daw_programacion.Unit1_tarea_ejercicio;
+
+public enum Turno {
+    MAÑANA, TARDE, NOCHE
+}
