1bab37d9b3ae17b03fa4585b6f25e9ab807e87bb
soheeparklee <soheeparklee@gmail.com>
2025-11-05 19:41:56 +0100

    Unit4_Album

--- src/main/java/com/example/daw_programacion/Unit3_class_album/PruebaAlbum.java
+++ src/main/java/com/example/daw_programacion/Unit3_class_album/PruebaAlbum.java
@@ -9,10 +9,22 @@ public class PruebaAlbum {
 
         Album album3 = new Album(album2, Soporte.CD);
 
-        System.out.println(album1.toString());
-        System.out.println(album3.toString());
+        //System.out.println(album1.toString());
+        //System.out.println(album3.toString());
 
         System.out.println("Precio total (sin IVA): "+ album1.calcularPrecio(album2) + " euros");
         System.out.println("Precio total (con 16% IVA): " + album1.calcularPrecio(album2, 16) + " euros");
+
+        Album[] albumsArray = new Album[3];
+        albumsArray[0] = album1;
+        albumsArray[1] = album2;
+        albumsArray[2] = new Album("Hola Hola", "Hola productions", "Rojo", 2012, Soporte.DISCO, 5, 10.99, Genero.POP, true);
+
+        //get the sum of price of array
+        float sum = 0;
+        for (Album album : albumsArray) {
+            sum += album.getPrecio();
+        }
+        System.out.println(sum);
     }
 }
