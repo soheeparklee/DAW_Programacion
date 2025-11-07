43f0be9e905c46c1461bd66514cb8c33e9387399
Vespertino <soheeparklee@gmail.com>
2025-11-07 19:48:42 +0100

    Unit4_MultiDimensionArray_break

--- src/main/java/com/example/daw_programacion/Unit4_Array/MultiDimensionArray.java
+++ src/main/java/com/example/daw_programacion/Unit4_Array/MultiDimensionArray.java
@@ -4,6 +4,7 @@ public class MultiDimensionArray {
     public static void main(String[] args) {
         int [][] matriz = {{2, 3, 4, 5}, {4, 2, 1}, {7, 2}};
 
+        /*
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) { //remember j should be smaller than matriz[i].length
                 System.out.print(matriz[i][j] + " ");
@@ -11,21 +12,43 @@ public class MultiDimensionArray {
             System.out.println();
         }
 
+         */
+
         /*
         2 3 4 5
         4 2 1
         7 2
          */
 
+        boolean find = false;
+        for (int i = 0; i < matriz.length && !find; i++) {
+            for (int j = 0; j < matriz[i].length && !find; j++) { //remember j should be smaller than matriz[i].length
+
+                if(matriz[i][j] == 5){
+                    find = true;
+                }
+
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // 2 3 4 5
+
         for (int[] innerArr : matriz) {
             for (int i : innerArr) {
+
+                if(i == 5){
+                    break;
+                }
+
                 System.out.print(i);
             }
             System.out.println();
         }
 
         /*
-        2345
+        234
         421
         72
          */
