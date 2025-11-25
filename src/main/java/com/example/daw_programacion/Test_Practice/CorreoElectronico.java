3bffcdd291fac798efc91124b6eba93fb13ee7b9
Vespertino <soheeparklee@gmail.com>
2025-11-25 17:47:10 +0100

    Correo

--- src/main/java/com/example/daw_programacion/Test_Practice/CorreoElectronico.java
+++ src/main/java/com/example/daw_programacion/Test_Practice/CorreoElectronico.java
@@ -1,15 +1,30 @@
 package com.example.daw_programacion.Test_Practice;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CorreoElectronico {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Pone linea");
         String input = sc.nextLine();
-
-
-
         String[] correos = new String[5];
+
+        int count = 0;
+        Pattern pattern = Pattern.compile("[\\w.%+-]+@[\\w.-]+\\.+[a-zA-Z]{2,4}");
+        Matcher matcher = pattern.matcher(input);
+
+        while(count < correos.length){
+            while(matcher.find()){
+                correos[count] = matcher.group();
+                count++;
+            }
+        }
+
+        for (String correo : correos) {
+            System.out.println(correo);
+        }
 
     }
 }
