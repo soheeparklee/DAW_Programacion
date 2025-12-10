0a58e814b8419ca3b19106812cf9025dd91aafe6
Vespertino <soheeparklee@gmail.com>
2025-12-10 20:14:08 +0100

    Ejercicio Sencillo

--- src/main/java/com/example/daw_programacion/Collections/Ejercicio_sencillo.java
+++ src/main/java/com/example/daw_programacion/Collections/Ejercicio_sencillo.java
@@ -1,6 +1,7 @@
 package com.example.daw_programacion.Collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -76,7 +77,23 @@ public class Ejercicio_sencillo {
         System.out.println(integerArrayList);
 
 */
-        //3.
+        //3. Persona
+        ArrayList<Persona> personaArrayList = new ArrayList<>();
+        Persona p1 = new Persona("1", "Park", "Park", "male", 12, 40.0);
+        Persona p2 = new Persona("2", "Dark", "Dark", "male", 12, 40.0);
+        Persona p3 = new Persona("3", "Cark", "Cark", "male", 12, 40.0);
+        Persona p4 = new Persona("4", "Sark", "Sark", "male", 12, 40.0);
+        Persona p5 = new Persona("5", "Aark", "Aark", "male", 12, 40.0);
+
+        personaArrayList.add(p1);
+        personaArrayList.add(p2);
+        personaArrayList.add(p3);
+        personaArrayList.add(p4);
+        personaArrayList.add(p5);
+
+
+        Collections.sort(personaArrayList); //Aark - Cark - Dark - Park - Sark
+        System.out.println( "personaArrayList" +  personaArrayList);
 
     }
     public static void agregar(ArrayList<Integer> arrList, Integer num) {
--- src/main/java/com/example/daw_programacion/Collections/Persona.java
+++ src/main/java/com/example/daw_programacion/Collections/Persona.java
@@ -1,21 +1,31 @@
 package com.example.daw_programacion.Collections;
 
-public class Persona {
+public class Persona implements Comparable<Persona>{
     String dni;
-    String apelido;
+    String apellido;
     String nombres;
     String sexo;
     Integer edad;
     Double peso;
 
-    public Persona(String dni, String apelido, String nombres, String sexo, Integer edad, Double peso) {
+    public Persona(String dni, String apellido, String nombres, String sexo, Integer edad, Double peso) {
         this.dni = dni;
-        this.apelido = apelido;
+        this.apellido = apellido;
         this.nombres = nombres;
         this.sexo = sexo;
         this.edad = edad;
         this.peso = peso;
     }
 
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "apellido='" + this.apellido + '\'' +
+                '}';
+    }
 
+    @Override
+    public int compareTo(Persona o) {
+        return this.apellido.charAt(0) > o.apellido.charAt(0) ? 1 : (this.apellido.charAt(0) == o.apellido.charAt(0)) ? 0 : -1;
+    }
 }
