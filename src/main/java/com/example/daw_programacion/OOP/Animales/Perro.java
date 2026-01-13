689cb0e18e87894d160a4edb4a5742fb60b1fd2a
Vespertino <soheeparklee@gmail.com>
2026-01-13 16:01:06 +0100

    Animal toString

--- src/main/java/com/example/daw_programacion/OOP/Animales/Perro.java
+++ src/main/java/com/example/daw_programacion/OOP/Animales/Perro.java
@@ -12,7 +12,27 @@ public class Perro extends Animal{
         super(nombre, patas);
         this.tipo = tipo;
     }
-/*
+
+    // case1: perro and animal both have toString() overrided
+/*    @Override
+    public String toString() {
+        return "Perro{" +
+                "tipo='" + tipo + '\'' +
+                "} " + super.toString();
+    }*/
+//    Animal{nombre='animal', patas=10}
+//    Perro{tipo='type1'} Animal{nombre='pepe', patas=4}
+// however, super.toString() part will print Animal...which is parent class
+
+// case2. only Animal has toString(), perro did not override
+    //then, when we run  System.out.println(perro);
+    // we will see Animal() parent class's toSting() executed
+    // which means, perro class attributes that Animal class does not have will not be printed.
+    // Animal{nombre='pepe', patas=4}
+
+
+
+    /*
     // hacerSonido() does not exist
     // then execute the parent(Animal) class hacerSonido()
     // will print "Sonido generico"
